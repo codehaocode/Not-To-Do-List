84fdf154018f3869e30740bb6e94b9eb0e12cf0e
Yuhao Zhong <Hao@Yuhaos-MacBook-Pro.local>
2020-08-27 14:18:23 +0200

    Create a new activity to show list detail

---
+++ app/src/main/java/net/atomday/nottodolist/ListDetailActivity.kt
@@ -0,0 +1,11 @@
+package net.atomday.nottodolist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_detail)
+    }
+}
